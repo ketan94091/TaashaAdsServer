21af84bf219a7ef678c39f9559e9b103b0dccdc2
Ketan Patel <ketan.taasha@gmail.com>
2021-01-13 18:20:21 +0530

    Add image Id

--- TaashaAdsLib/src/main/java/com/example/taashaadslib/AdSerever/LoadImageActivity.java
+++ TaashaAdsLib/src/main/java/com/example/taashaadslib/AdSerever/LoadImageActivity.java
@@ -3,14 +3,25 @@ package com.example.taashaadslib.AdSerever;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.taashaadslib.R;
 
 public class LoadImageActivity extends AppCompatActivity {
 
+    private ImageView imgLocalImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_image);
+
+        setId();
+    }
+
+    private void setId() {
+
+        imgLocalImage = (ImageView)findViewById(R.id.imgLocalImage);
+        
     }
 }
