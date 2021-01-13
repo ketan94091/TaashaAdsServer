696fabbad37bc918743cbe8008477ccef45a8edc
Ketan Patel <ketan.taasha@gmail.com>
2021-01-13 16:49:10 +0530

    Create test file
    Create test file

---
+++ TaashaAdsLib/src/main/java/com/example/taashaadslib/LoggingClass.java
@@ -0,0 +1,15 @@
+package com.example.taashaadslib;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class LoggingClass extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logging);
+    }
+}
