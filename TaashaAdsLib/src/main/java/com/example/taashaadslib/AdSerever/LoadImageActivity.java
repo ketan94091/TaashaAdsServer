56372d1e7cb280b5056ede2fb97de01ba6651bd7
Ketan Patel <ketan.taasha@gmail.com>
2021-01-15 16:30:16 +0530

    url image

--- TaashaAdsLib/src/main/java/com/example/taashaadslib/AdSerever/LoadImageActivity.java
+++ TaashaAdsLib/src/main/java/com/example/taashaadslib/AdSerever/LoadImageActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -14,7 +15,10 @@ public  class LoadImageActivity  {
 
     public static void loadAdsView(ImageView imgView){
 
-        imgView.setImageResource(R.drawable.test);
+        //imgView.setImageResource(R.drawable.test);
+
+        Uri uri =  Uri.parse( "https://i.imgur.com/PZ4rb.jpg" );
+        imgView.setImageURI(uri);
 
     }
 
