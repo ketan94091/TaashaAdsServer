af5ff435e8295d37c27c03449a9f61561018640c
Ketan Patel <ketan.taasha@gmail.com>
2021-01-15 18:06:29 +0530

    add counter

--- TaashaAdsLib/src/main/java/com/example/taashaadslib/AdSerever/LoadImageActivity.java
+++ TaashaAdsLib/src/main/java/com/example/taashaadslib/AdSerever/LoadImageActivity.java
@@ -3,11 +3,13 @@ package com.example.taashaadslib.AdSerever;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -15,12 +17,56 @@ import com.example.taashaadslib.R;
 
 import java.net.URL;
 
-public  class LoadImageActivity  {
+public class LoadImageActivity {
 
 
-    public static void loadAdsView(ImageView imgView , Activity mActivity){
+    private static Handler handler;
+    public static int count = 1;
 
-        imgView.setImageResource(R.drawable.whatapp);
+    public void starttimer() {
+
+
+    }
+
+    public static void loadAdsView(ImageView imgView) {
+
+
+        switch (count) {
+
+            case 1:
+                imgView.setImageResource(R.drawable.facebook);
+                break;
+            case 2:
+                imgView.setImageResource(R.drawable.whatapp);
+                break;
+            case 3:
+                imgView.setImageResource(R.drawable.instagram);
+                break;
+            case 4:
+                imgView.setImageResource(R.drawable.appdate);
+                break;
+            case 5:
+                imgView.setImageResource(R.drawable.snapchat);
+                break;
+        }
+
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                count++;
+
+                if(count == 6){
+                    count=1;
+                }
+
+                loadAdsView(imgView);
+            }
+        }, 1000);
+
+
 
         /*Uri uri =  Uri.parse( "https://i.imgur.com/PZ4rb.jpg" );
         imgView.setImageURI(uri);*/
@@ -29,7 +75,6 @@ public  class LoadImageActivity  {
 
 
     }
-
 
 
 }
