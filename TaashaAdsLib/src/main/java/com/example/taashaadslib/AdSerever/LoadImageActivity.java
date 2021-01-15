89035ad177c6cd5cda251a07623e0891eac93239
Ketan Patel <ketan.taasha@gmail.com>
2021-01-15 17:31:53 +0530

    Add Glide

--- TaashaAdsLib/src/main/java/com/example/taashaadslib/AdSerever/LoadImageActivity.java
+++ TaashaAdsLib/src/main/java/com/example/taashaadslib/AdSerever/LoadImageActivity.java
@@ -2,23 +2,31 @@ package com.example.taashaadslib.AdSerever;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.taashaadslib.R;
+
+import java.net.URL;
 
 public  class LoadImageActivity  {
 
 
-    public static void loadAdsView(ImageView imgView){
+    public static void loadAdsView(ImageView imgView , Activity mActivity){
 
         //imgView.setImageResource(R.drawable.test);
 
-        Uri uri =  Uri.parse( "https://i.imgur.com/PZ4rb.jpg" );
-        imgView.setImageURI(uri);
+        /*Uri uri =  Uri.parse( "https://i.imgur.com/PZ4rb.jpg" );
+        imgView.setImageURI(uri);*/
+
+        Glide.with(mActivity).load("https://i.imgur.com/PZ4rb.jpg").into(imgView);
+
 
     }
 
